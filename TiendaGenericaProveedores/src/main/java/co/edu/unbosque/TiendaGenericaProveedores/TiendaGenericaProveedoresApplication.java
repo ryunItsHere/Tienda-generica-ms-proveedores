732474fc8b0a21843a6bfb30b394fdad752d385d
Ana Flechas <aflechast@unbosque.edu.co>
2026-03-08 21:00:23 -0500

    primer commit

---
+++ TiendaGenericaProveedores/src/main/java/co/edu/unbosque/TiendaGenericaProveedores/TiendaGenericaProveedoresApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.TiendaGenericaProveedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGenericaProveedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGenericaProveedoresApplication.class, args);
+	}
+
+}
---
+++ TiendaGenericaProveedores/src/test/java/co/edu/unbosque/TiendaGenericaProveedores/TiendaGenericaProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.TiendaGenericaProveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGenericaProveedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
