93fb52c9d567b22f7582b24a561b3c3074005a71
Ana Flechas <aflechast@unbosque.edu.co>
2026-03-08 21:40:44 -0500

    Repositorio proveedor

---
+++ TiendaGenericaProveedores/src/main/java/co/edu/unbosque/TiendaGenericaProveedores/repository/ProveedorRepository.java
@@ -0,0 +1,19 @@
+package co.edu.unbosque.TiendaGenericaProveedores.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.unbosque.TiendaGenericaProveedores.model.Proveedor;
+
+
+@Repository
+public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+
+    Optional<Proveedor> findByNombre(String nombre_proveedor);
+    boolean existsByNitIgnoreCase(String nit_proveedor);
+    List<Proveedor> findByCiudad(String ciudad_proveedor);
+    List<Proveedor> findByNombreContainingIgnoreCase(String nombre_proveedor);
+}
